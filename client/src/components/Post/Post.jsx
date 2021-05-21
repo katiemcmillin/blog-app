5a978a41ca2038c1cd60ef15a8d56600c25cc4f0
Anss Chaudhry <ansschaudhry@ansss-mac-mini.lan>
2021-05-21 16:51:03 -0400

    post and postcard done

---
+++ client/src/components/Post/Post.jsx
@@ -0,0 +1,18 @@
+import "./Post.css";
+import { Link } from "react-router-dom";
+
+const Post = (props) => {
+  return (
+    <>
+      <Link className="post" to={`/posts/${props._id}`}>
+        <img className="post-image" src={props.imgURL} alt={props.author} />
+        <div className="post-title">{props.title}</div>
+        <div className="post-author">{props.author}</div>
+        <div className="post-date">{props.date}</div>
+        <div className="post-content">{props.content}</div>
+      </Link>
+    </>
+  );
+};
+
+export default Product;
