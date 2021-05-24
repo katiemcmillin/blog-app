7481a5fa39747f773963bd1fd899bdd88cf80392
Katie McMillin <katiemcmillin@Katies-MBP.home>
2021-05-24 14:23:57 -0400

    Posts screen done

---
+++ client/src/screens/Posts/Posts.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import './Products.css'
+
+import Post from "../..components/Post/Post"
+import Layout from "../../components/Layout/Layout"
+import { getPosts } from "../..services/posts"
+
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const allPosts = await getPosts()
+      setPosts(allPosts)
+    }
+    fetchPosts()
+  }, [])
+  return (
+    <Layout>
+      <div className="posts">
+        {posts.map((post, index) => {
+          return (
+        <Post
+          _id={post._id}
+          author={post.author}
+          title={post.title}
+          content={post.content}
+          key={index}
+          imgURL={post.imgURL}
+          alt={post.author}
+ 
+        />
+          )
+
+})}
+      </div>
+  </Layout>
+)
+
+}
+export default Posts
